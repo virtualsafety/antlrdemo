113095c7453cc48db527081ac300de1bd3610f7d
virtualsafety <virtualsafety@users.noreply.github.com>
2021-07-27 11:18:03 +0800

    Update jobStatus.kt

--- jobStatus.kt
+++ jobStatus.kt
@@ -8,10 +8,12 @@ fun main() {
         this.cancel()
     }
     val job2 = coroutineScope.launch { delay(1000L) }
+    job2.invokeOnCompletion { println("Job2 completed") }
 
     println("Job 1 state: ${job1.status()}")
     println("Job 2 state: ${job2.status()}")
     println("Parent job is active: ${coroutineScope.isActive}")
+    Thread.sleep(2000)
 }
 
 fun Job.status(): String = when {
